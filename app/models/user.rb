e1f3fad0c1f51fd90266c5069cba3f438be8566e
AlexPashchenko <sashka.gag@gmail.com>
2016-12-28 10:47:56 +0200

    Users roles created

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,5 +33,5 @@ class User < ApplicationRecord
   has_many :comments
   has_attached_file :avatar, styles: { medium: "100x100>", thumb: "50x50>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
-  enumerize :role, in: [:admin, :user]
+  enumerize :role, in: [:admin, :user], default: :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
 
   devise_for :users
- root 'mainpage#index'
+  root 'mainpage#index'
 
   resources :categories, only: [:create, :destroy, :update]
 
 
   resources :posts do
     resources :tracks
-   resources :comments, only: [:create, :destroy]
- end
+    resources :comments, only: [:create, :destroy]
+  end
 end
